830280fdb9141003638baff1f83d0ef05cec2e15
Nikhil <nikhil.bhatt.000@gmail.com>
2021-10-01 18:53:19 +0530

    feature added for input

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ let fahrenheit=document.getElementById('fahrenheit');
 function celToFar(){
     let output=(parseFloat(celsius.value)*9/5)+32;
     fahrenheit.value=parseFloat(output.toFixed(2));
+    var x = document.getElementsByTagName("BODY")[0];
+    x.style.backgroundColor = "rgb("+Math.round(Math.random()*255)+","+Math.round(Math.random()*255)+","+Math.round(Math.random()*255)+")";
 }
 
 function farToCel(){
     let output=(parseFloat(fahrenheit.value)-32)*5/9;
     celsius.value=parseFloat(output.toFixed(2));
+    var x = document.getElementsByTagName("BODY")[0];
+    x.style.backgroundColor = "rgb("+Math.round(Math.random()*255)+","+Math.round(Math.random()*255)+","+Math.round(Math.random()*255)+")";
 }
